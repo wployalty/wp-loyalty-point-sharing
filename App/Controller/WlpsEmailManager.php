7157e04655aed6bc9d7e11c561d9e598a3622835
siva subramanian <sivasubramanianj2002@gmail.com>
2025-10-24 11:12:15 +0530

    email override issue fixed

--- App/Controller/WlpsEmailManager.php
+++ App/Controller/WlpsEmailManager.php
@@ -8,6 +8,7 @@ use Wlps\App\Emails\PointTransferReceiverEmail;
 class WlpsEmailManager {
 	public static function init() {
 		add_filter( "woocommerce_email_classes", [ self::class, 'addEmailClass' ] );
+		add_filter( 'woocommerce_template_directory', [ self::class, 'addTemplateDirectory' ], 10, 2 );
 	}
 
 	public static function addEmailClass( $emails ) {
@@ -19,5 +20,20 @@ class WlpsEmailManager {
 		}
 
 		return $emails;
+	}
+
+	public static function addTemplateDirectory( $template_dir, $template ) {
+		$my_templates = [
+			'emails/point-transfer-sender.php',
+			'emails/point-transfer-receiver.php',
+			'emails/plain/point-transfer-sender.php',
+			'emails/plain/point-transfer-receiver.php'
+		];
+
+		if ( in_array( $template, $my_templates ) ) {
+			return 'wployalty';
+		}
+
+		return $template_dir;
 	}
 }
